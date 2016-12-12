67ee56bffc7206739b152b51ad66637bbd3e80f7
vAsSiLyB <marcbreil2014@gmail.com>
2016-12-12 15:46:56 +0100

    Add files via upload

---
+++ plugins/AW_Etude/lang/aw_etude_fr.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aw_etude_titre' => 'Arte Workflow Etude',
+		
+	// CE
+	'CE_chart_label' => 'Afficher Graphique d\'activité / Charge de travail (Id)',
+	'CE_graph_label' => 'Afficher Graph Structurel (Id)',
+	'CE_numero_ordre_label' => 'Numéro  d\'ordre',
+		
+	// I
+	'icone_creer_diapo' => 'Nouvelle Diapo',
+		
+	// L
+	'label_editer_diapo' => 'Editer la diapositive',
+		
+	// T
+	'titre_diapositives' => 'Diapositives'
+);
